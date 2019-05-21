1edc039d4779104d29b8cf7576a9b86e9b13faa7
Julia Soboleva <julia.soboleva26@gmail.com>
2019-05-21 14:31:06 +0300

    Removed report landscape setting from extra settings (too many columns possible)

--- app/src/main/java/co/smartreceipts/android/settings/widget/PreferenceHeaderReportOutputFragment.java
+++ app/src/main/java/co/smartreceipts/android/settings/widget/PreferenceHeaderReportOutputFragment.java
@@ -9,7 +9,7 @@ public class PreferenceHeaderReportOutputFragment
         extends AbstractPreferenceHeaderFragment {
 
     public static List<Integer> EXTRA_SETTINGS_KEYS = Arrays.asList(R.string.pref_output_custom_csv_key,
-            R.string.pref_output_custom_pdf_key, R.string.pref_output_receipts_landscape_key,
+            R.string.pref_output_custom_pdf_key, /*R.string.pref_output_receipts_landscape_key,*/
             R.string.pref_output_pdf_page_size_key, R.string.pref_output_preferred_language_key);
 
     @Override
